fafbd213fbe7a653f6ebaeedfd195ea8406542a4
Cliff Su <stu01509@yahoo.com.tw>
2024-05-16 15:06:20 +0800

    [ISSUE-#1] chore: add the emotion, react-transition-group, svgr

--- jest.config.ts
+++ jest.config.ts
@@ -7,11 +7,24 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   clearMocks: true,
   coverageProvider: 'babel',
-  moduleNameMapper: { '^@/(.*)$': '<rootDir>/$1' },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
   preset: 'ts-jest',
   setupFilesAfterEnv: ['<rootDir>/jest/jest-setup.ts'],
   testEnvironment: 'jsdom',
   testMatch: ['**/?(*.)+(spec).[tj]s?(x)'],
 };
 
-export default createJestConfig(customJestConfig);
+const jestConfig = async () => {
+  const nextJestConfig = await createJestConfig(customJestConfig)();
+  return {
+    ...nextJestConfig,
+    moduleNameMapper: {
+      '\\.svg$': '<rootDir>/__mocks__/svg.ts',
+      ...nextJestConfig.moduleNameMapper,
+    },
+  };
+};
+
+export default jestConfig;
